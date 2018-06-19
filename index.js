d19d3d1997132ae53598a22e7a2dbeb3fbc076ba
Philliph Drummond <oilsinwater@gmail.com>
2018-06-18 23:04:52 -0700

    feat: starts server with first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Primary file for the API
+ */
+
+// Dependencies
+ 
+const http = require('http');
+
+
+ // The server shold respond to tall requests with a string
+let server = http.createServer(function (req, res) {
+    res.end('Hello World\n');
+});
+
+
+ // Start the server, and have it listen on port 3000
+ server.listen(3000, function (){
+     console.log('The server is listening on port 3000 now');
+ });
