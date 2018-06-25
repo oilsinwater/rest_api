c58b2cedc981bf9cb3c47d004ae38ede0332aa60
Philliph Drummond <oilsinwater@gmail.com>
2018-06-24 22:53:27 -0700

    feat: creates config.js file

---
+++ config.js
@@ -0,0 +1,28 @@
+
+
+// Object container for exporting all the environments
+
+let environments = {};
+
+
+// Staging (default) environment
+environments.staging = {
+    'port' : 8080,
+    'envName' : 'staging'
+};
+
+// Production environment
+environments.production = {
+    'port' : 3000,
+    'envName' : 'production'
+};
+
+// Determine which environments was passed as a cli argument
+let currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// Check if current environment is set to one of the keys on the environment object
+let environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+
+// Export the module
+module.exports = environmentToExport;
